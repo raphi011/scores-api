b4ed9d262f25ecdbafb71b3c5d76c0ffdcfb5de3
Raphael Gruber <raphi011@gmail.com>
2017-11-27 21:56:27 +0100

    fix: production https link

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const prod = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  'process.env.BACKEND_URL': prod ? 'http://scores.raphi011.com' : 'http://localhost:3000'
+  'process.env.BACKEND_URL': prod ? 'https://scores.raphi011.com' : 'http://localhost:3000'
 }
