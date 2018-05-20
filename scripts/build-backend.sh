83a0288bd69792df5f6ab379b99cd25a2ed0da73
Raphael Gruber <raphi011@gmail.com>
2018-05-20 13:15:01 +0200

    chore: fix build script paths

--- scripts/build-backend.sh
+++ scripts/build-backend.sh
@@ -4,4 +4,4 @@ PLATFORM=$1
 URL=$2
 
 sudo docker rm -f scores-backend-${PLATFORM}
-sudo docker build -t raphi011/scores-backend-${PLATFORM} .. --build-arg app_env=production --build-arg backend_url=${URL}
+sudo docker build -t raphi011/scores-backend-${PLATFORM} . --build-arg app_env=production --build-arg backend_url=${URL}
--- scripts/build-frontend.sh
+++ scripts/build-frontend.sh
@@ -4,4 +4,4 @@ PLATFORM=$1
 URL=$2
 
 sudo docker rm -f scores-frontend-${PLATFORM}
-sudo docker build -t raphi011/scores-frontend-${PLATFORM} ../web --build-arg backend_url=${URL}
+sudo docker build -t raphi011/scores-frontend-${PLATFORM} ./web --build-arg backend_url=${URL}
