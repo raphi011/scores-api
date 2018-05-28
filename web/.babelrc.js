ae59b27c1e6d4e34e56f8d00f13ad23d3e85ec3e
Raphael Gruber <raphi011@gmail.com>
2018-05-28 21:56:02 +0200

    test: fix tests

--- web/.babelrc.js
+++ web/.babelrc.js
@@ -3,4 +3,9 @@ const env = require('./env-config');
 module.exports = {
   presets: ['next/babel'],
   plugins: ['transform-flow-strip-types', ['transform-define', env]],
+  env: {
+    test: {
+      presets: [['next/babel', { 'preset-env': { modules: 'commonjs' } }]],
+    },
+  },
 };
