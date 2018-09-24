3ac7e90aff2767ad2aa3a6bdc23b44e8cf7034c8
Raphael Gruber <raphi011@gmail.com>
2018-09-24 07:16:56 +0200

    feat: playground for ui rework

---
+++ playground/src/components/Breadcrumbs.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+import * as Theme from "../theme";
+
+const Breadcrumbs = () => (
+    <span>
+        <a href="#">Volleynet</a> / <a href="#">Tournaments</a>
+    </span>
+);
+
+
+export default Breadcrumbs;
---
+++ playground/src/theme.js
@@ -0,0 +1,5 @@
+export const c1 = "#96CEB4";
+export const c2 = "#FFEEAD";
+export const c3 = "#FF6F69";
+export const c4 = "#FFCC5C";
+export const c5 = "#AAD8B0";
