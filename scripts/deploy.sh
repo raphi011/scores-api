84aaf8a95e2fb9d529ec8f0fdcbee6c85bc19919
Raphael Gruber <raphi011@gmail.com>
2018-11-28 22:29:27 +0100

    chore: travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --build
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml build
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml down
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml -d --force-recreate --remove-orphans up
