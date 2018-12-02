40155c3d0e1b63ab19cb39fd52c76b66eab02ab1
Raphael Gruber <raphi011@gmail.com>
2018-12-02 14:14:54 +0100

    chore: completely separate docker-compose files again

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml build
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml down
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d --force-recreate --remove-orphans 
+docker-compose -f docker-compose.prod.yml build
+docker-compose -f docker-compose.prod.yml down
+docker-compose -f docker-compose.prod.yml up -d --force-recreate --remove-orphans 
