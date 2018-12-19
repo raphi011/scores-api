674914536d35f40495581babcf28a53bbf318010
Raphael Gruber <raphi011@gmail.com>
2018-12-19 23:16:55 +0100

    feat: improve edit user dialog

--- web/components/admin/EditUserDialog.tsx
+++ web/components/admin/EditUserDialog.tsx
@@ -1,12 +1,23 @@
 import React, { ChangeEvent } from 'react';
 
-import { Dialog, DialogTitle, TextField } from '@material-ui/core';
+import { Dialog, DialogTitle, TextField, Button } from '@material-ui/core';
+import { createStyles, withStyles } from '@material-ui/core/styles';
+
 import { User } from '../../types';
+
+const styles = createStyles({
+  userForm: {
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '10px',
+  },
+});
 
 type Props = {
   user?: User;
   open: boolean;
   isNew: boolean;
+  classes: any;
 
   email: string;
   password: string;
@@ -16,31 +27,42 @@ type Props = {
   onChangePassword: (event: ChangeEvent<HTMLInputElement>) => void;
 };
 
-export default ({
-  isNew,
-  onClose,
-  open,
-  email,
-  onChangeEmail,
-  password,
-  onChangePassword,
-}: Props) => {
-  const title = isNew ? 'New User' : 'Edit User';
+export default withStyles(styles)(
+  ({
+    isNew,
+    onClose,
+    open,
+    email,
+    onChangeEmail,
+    password,
+    onChangePassword,
+    classes,
+  }: Props) => {
+    const title = isNew ? 'New User' : 'Edit User';
 
-  return (
-    <Dialog onClose={onClose} open={open}>
-      <DialogTitle>{title}</DialogTitle>
-      <form noValidate autoComplete="off">
-        <TextField label="Email" value={email} onChange={onChangeEmail} />
-        <TextField
-          label="Password"
-          type="password"
-          margin="normal"
-          value={password}
-          onChange={onChangePassword}
-        />
-      </form>
-      <div />
-    </Dialog>
-  );
-};
+    return (
+      <Dialog onClose={onClose} open={open}>
+        <DialogTitle>{title}</DialogTitle>
+        <form className={classes.userForm} noValidate autoComplete="off">
+          <TextField
+            label="Email"
+            disabled={!isNew}
+            value={email}
+            onChange={onChangeEmail}
+          />
+          <TextField
+            label="Password"
+            type="password"
+            margin="normal"
+            value={password}
+            onChange={onChangePassword}
+          />
+          <Button color="primary" variant="contained">
+            Submit
+          </Button>
+        </form>
+        <div />
+      </Dialog>
+    );
+  },
+);
