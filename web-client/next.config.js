05a33eb404a927d8df3bf8029d16371d46571de4
Raphael Gruber <raphi011@gmail.com>
2019-01-28 19:20:53 +0100

    chore: fix production build

--- web-client/next.config.js
+++ web-client/next.config.js
@@ -1,16 +1,11 @@
 const webpack = require('webpack');
-const childProcess = require('child_process');
 const withTypescript = require('@zeit/next-typescript');
-
-const version = childProcess
-  .execSync('git describe --always --long')
-  .toString();
 
 module.exports = withTypescript({
   webpack(config) {
     config.plugins.push(
       new webpack.DefinePlugin({
-        VERSION: JSON.stringify(version),
+        VERSION: JSON.stringify(process.env.VERSION),
       }),
     );
 
