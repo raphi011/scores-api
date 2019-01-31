90c1dc03fa73e57c1b5a506b34631b966d4790fa
Raphael Gruber <raphi011@gmail.com>
2019-01-31 19:35:27 +0100

    fix: user is undefined errors while logging out

--- web-client/pages/logout.tsx
+++ web-client/pages/logout.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
 
-import { Context, redirectWithContext } from '../containers/AuthContainer';
+import Router from 'next/router';
+import { Store } from 'redux';
+
+import { Context } from '../containers/AuthContainer';
 import { logoutAction } from '../redux/auth/actions';
 
 interface Props {
-  logout: () => Promise<void>;
+  store: Store;
 }
 
 export default class Logout extends React.Component<Props> {
   static async getInitialProps(ctx: Context) {
     const { store } = ctx;
 
-    await store.dispatch(logoutAction());
-    redirectWithContext(ctx, '/login');
+    return { store };
+  }
 
-    return {};
+  async componentDidMount() {
+    const { store } = this.props;
+
+    await store.dispatch(logoutAction());
+    Router.push('/login');
   }
 
   render() {
