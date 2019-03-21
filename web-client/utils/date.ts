a5b7f8bc1303dfa6b8cb4276fbf3ed810aacd6ad
Raphael Gruber <raphael.gruber@brainloop.com>
2019-03-21 09:34:50 +0100

    fix: tournament list grouping

--- web-client/utils/date.ts
+++ web-client/utils/date.ts
@@ -25,6 +25,6 @@ export function sameDay(d1: Date, d2: Date): boolean {
   return (
     d1.getFullYear() === d2.getFullYear() &&
     d1.getMonth() === d2.getMonth() &&
-    d1.getDay() === d2.getDay()
+    d1.getDate() === d2.getDate()
   );
 }
