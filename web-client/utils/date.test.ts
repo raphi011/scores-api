1b3343de6dbe9c9e51c3ebfa41f4176cd80e5906
Raphael Gruber <raphi011@gmail.com>
2019-06-03 21:59:06 +0200

    fix: date test

--- web-client/utils/date.test.ts
+++ web-client/utils/date.test.ts
@@ -3,5 +3,5 @@ import { formatDate } from './date';
 const date = new Date(2017, 0, 1);
 
 test('formatDate', () => {
-  expect(formatDate(date)).toBe('1.1.2017');
+  expect(formatDate(date)).toBe('Sunday, 1.1.2017');
 });
