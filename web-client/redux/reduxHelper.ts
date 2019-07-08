3adc1987645ff580e03ed2c715c026bfa85ccec1
Raphael Gruber <raphi011@gmail.com>
2019-07-08 22:15:10 +0200

    fix: frontend lint

--- web-client/redux/reduxHelper.ts
+++ web-client/redux/reduxHelper.ts
@@ -1,13 +1,11 @@
 import { Action } from 'redux';
-// import { Store } from './store';
 
-/* eslint-disable import/prefer-default-export */
 export function createReducer(
   initialState: any,
   handlers: { [actionType: string]: (state: any, action: any) => object },
 ) {
   return function reducer(state = initialState, action: Action) {
-    if (handlers.hasOwnProperty(action.type)) {
+    if (Object.prototype.hasOwnProperty.call(handlers, action.type)) {
       return handlers[action.type](state, action);
     }
 
